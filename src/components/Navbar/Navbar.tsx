43634fbad14167464132ce849f82924234f551de
sofiyaOsadchi <sofiyahason@gmail.com>
2024-05-11 23:07:11 +0300

    nav

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,45 +1,57 @@
-import { FaCaretDown, FaHeart, FaHome } from "react-icons/fa";
+import { FaCaretDown, FaBars, FaTimes } from "react-icons/fa";
 import { RxAvatar } from "react-icons/rx";
 import { NavLink, useNavigate } from "react-router-dom";
 import DarkModeToggle from "../DarkModeToggle/DarkModeToggle";
 import "./Navbar.scss";
 import { useAuth } from "../../contexts/AuthContext";
-import Search from "../../routes/Search";
+import { useState } from "react";
+/* import {
+  Navbar as MTNavbar, MobileNav, Typography, Button, IconButton,} from '@material-tailwind/react'; */
+
 
 const Navbar = () => {
   const { isLoggedIn, user, logout } = useAuth();
-
-
   const navigate = useNavigate();
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+
+  
   return (
-    <nav className="site-navbar">
+    <nav className="site-navbar ">
       <div className="nav-left">
-        <NavLink to="/" className="brand">
-          <FaHome />
+
+        <button className="burger-icon" onClick={toggleMenu}>
+          {menuOpen ? <FaTimes /> : <FaBars />}
+        </button>
+        <div className={`menu-links ${menuOpen ? "open" : ""}`}>
+        
+          <NavLink to="/" className="brand" onClick={toggleMenu}>
           Home
         </NavLink>
-        <NavLink to="/about" className="about">
+          <NavLink to="/about" className="about" onClick={toggleMenu}>
           About
         </NavLink>
 
-        {isLoggedIn && <NavLink to="/favorites"><span style={{ display: 'inline-block' }}>{/* <FaHeart /> */}Favorites</span></NavLink>}
+          {isLoggedIn && <NavLink to="/favorites" onClick={toggleMenu}><span style={{ display: 'inline-block' }}>{}Favorites</span></NavLink>}
         
         {isLoggedIn && user?.isBusiness && (
-          <NavLink to="/my-cards">My Cards</NavLink>
+            <NavLink to="/my-cards" onClick={toggleMenu}>My Cards</NavLink>
         )}
 
         {isLoggedIn && user?.isBusiness && (
-          <NavLink to="/create-card">Create Card</NavLink>
+            <NavLink to="/create-card" onClick={toggleMenu}>Create Card</NavLink>
         )}
+      </div>
       </div>
 
 
       <div className="nav-right">
         {!isLoggedIn && <NavLink to="/register">Register</NavLink>}
         {!isLoggedIn && <NavLink to="/login">Login</NavLink>}
-       
-
-        {/* {isLoggedIn && <NavLink to="/profile"> Profile</NavLink>} */}
 
         <div>
           {isLoggedIn && (
@@ -76,3 +88,8 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
+
+
+
+{/* {isLoggedIn && <NavLink to="/profile"> Profile</NavLink>} */ }
